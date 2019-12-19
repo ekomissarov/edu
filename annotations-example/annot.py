c406bd223a5cbbd446783e1175bf035e102beb86
Eugene Komissarov <ekom@cian.ru>
2019-12-19 18:53:56 +0300

    annotations

--- annotations-example/annot.py
+++ annotations-example/annot.py
@@ -3,4 +3,15 @@
 # 09-Aug-2016 https://www.python.org/dev/peps/pep-0526/ Syntax for Variable Annotations
 # 8-Sep-2017 https://www.python.org/dev/peps/pep-0563/ Postponed Evaluation of Annotations
 
+# https://habr.com/ru/company/lamoda/blog/432656/
 # https://www.youtube.com/watch?v=R2QtnebNZdo
+
+# from __future__ import annotations
+from typing import List
+
+
+def concat_them_all(strs: List[str]):
+    return ''.join(strs)
+
+
+print(concat_them_all(['1', '2']))
