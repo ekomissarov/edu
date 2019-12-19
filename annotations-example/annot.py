f89d251cf7279401444273e56074eeac38f556fe
Eugene Komissarov <ekom@cian.ru>
2019-12-19 18:28:54 +0300

    annotations

---
+++ annotations-example/annot.py
@@ -0,0 +1,6 @@
+# 2-Dec-2006 https://www.python.org/dev/peps/pep-3107/ Function Annotations
+# 29-Sep-2014 https://www.python.org/dev/peps/pep-0484/ Type Hints
+# 09-Aug-2016 https://www.python.org/dev/peps/pep-0526/ Syntax for Variable Annotations
+# 8-Sep-2017 https://www.python.org/dev/peps/pep-0563/ Postponed Evaluation of Annotations
+
+# https://www.youtube.com/watch?v=R2QtnebNZdo
