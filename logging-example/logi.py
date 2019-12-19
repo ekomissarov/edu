f536687fc0c0e981f65b4b0a1e767aa38f946305
Eugene Komissarov <ekom@cian.ru>
2019-12-19 15:59:01 +0300

    logging config

--- logging-example/logi.py
+++ logging-example/logi.py
@@ -1,7 +1,8 @@
 import logging
+import logging.config
 # https://webdevblog.ru/logging-v-python/
 
-def fun():
+def example1():
     val = "QKRQ!"
 
     logging.basicConfig(level=logging.DEBUG,
@@ -31,6 +32,40 @@ def fun():
         '''
 
 
+def example2():
+    '''
+        Использование Handlers
+    '''
+    logger = logging.getLogger(__name__)
+
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('file.log')
+    c_handler.setLevel(logging.WARNING)
+    f_handler.setLevel(logging.ERROR)
+
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+    logger.warning('This is a warning')
+    logger.error('This is an error')
+
+
+def example3():
+    logging.config.fileConfig(fname='log_config.txt', disable_existing_loggers=False)
+    # Get the logger specified in the file
+    logger = logging.getLogger(__name__)
+    logger.debug('This is a debug message')
+
+
 if __name__ == '__main__':
     print("="*30)
-    fun()
+    #example1()
+    #example2()
+    example3()
